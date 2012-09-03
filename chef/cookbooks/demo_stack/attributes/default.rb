36818440e85c8ddb0a772ba7f7b4ecfbfb4a23bd
Sean Porter <portertech@gmail.com>
2012-09-03 14:58:33 -0700

    logstash grep/grok to extract request and riak metrics

--- chef/cookbooks/demo_stack/attributes/default.rb
+++ chef/cookbooks/demo_stack/attributes/default.rb
@@ -22,9 +22,40 @@ override.logstash.server.inputs = [
 
 override.logstash.server.filters = [
   {
+    :grep => {
+      :type => "demo_stack",
+      :match => ["@message", "request :"],
+      :add_tag => ["request"],
+      :drop => false
+    }
+  },
+  {
+    :grep => {
+      :type => "demo_stack",
+      :match => ["@message", "riak :"],
+      :add_tag => ["riak"],
+      :drop => false
+    }
+  },
+  {
     :grok => {
       :type => "demo_stack",
-      :pattern => '%{TIMESTAMP_ISO8601:timestamp} \| %{WORD:application} \| %{WORD:severity}%{SPACE}\| \[%{DATA:thread}\] \| %{DATA:class} \| %{GREEDYDATA:message}'
+      :pattern => '%{TIMESTAMP_ISO8601:timestamp} \| %{WORD:application} \| %{WORD:severity}%{SPACE}\| \[%{DATA:thread}\] \| %{DATA:class} \| %{GREEDYDATA:message}',
+      :add_tag => ["grokked"]
+    }
+  },
+  {
+    :grok => {
+      :type => "demo_stack",
+      :tags => ["request", "grokked"],
+      :match => ["message", '%{WORD} \:%{WORD:request_method} %{URIPATH:request_uri} %{INT:response_code} \(%{INT:response_time}ms\)'],
+    }
+  },
+  {
+    :grok => {
+      :type => "demo_stack",
+      :tags => ["riak", "grokked"],
+      :match => ["message", '%{WORD} \:%{WORD:riak_operation} %{DATA:riak_key} \(%{INT:riak_time}ms\)'],
     }
   },
   {
